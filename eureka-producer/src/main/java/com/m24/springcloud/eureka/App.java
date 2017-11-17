2ea86ea65afbebedf199a49d7aab0e48b9c5c698
m2492565210 <2492565210@qq.com>
2017-11-17 09:16:16 +0800

    add bus

--- eureka-producer/src/main/java/com/m24/springcloud/eureka/App.java
+++ eureka-producer/src/main/java/com/m24/springcloud/eureka/App.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class App 
 {
+	@Value("${com.m24.constant.active}")
+	String active;
+	
     public static void main( String[] args )
     {
     	SpringApplication.run(App.class, args);
@@ -22,6 +27,6 @@ public class App
 	String port;
 	@RequestMapping("/hi")
 	public String home(@RequestParam String name) {
-		return "hi "+name+",i am from port:" +port;
+		return "hi "+name+",i am from port: " +port +", config: active=" + active;
 	}
 }
