028177c78a56961dd8a3ed0249e1ef1bf7b5d17f
m2492565210 <2492565210@qq.com>
2017-11-17 12:07:01 +0800

    final

--- microservice-turbine/src/main/java/com/m24/springcloud/eureka/App.java
+++ microservice-turbine/src/main/java/com/m24/springcloud/eureka/App.java
@@ -2,16 +2,12 @@ package com.m24.springcloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
+@EnableTurbine
 @EnableDiscoveryClient
-@EnableFeignClients
-@EnableHystrixDashboard
-@EnableCircuitBreaker
 public class App 
 {
     public static void main( String[] args )
